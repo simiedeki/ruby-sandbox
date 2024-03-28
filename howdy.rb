d7a9aa9877d91746dcaa0c4e2b493d1b630555aa
simiedeki <164128583+simiedeki@users.noreply.github.com>
2024-03-28 04:25:58 +0000

    initial

---
+++ howdy.rb
@@ -0,0 +1,12 @@
+my_string = "Hello, world!"
+pp my_string
+
+require "./goodbye.rb"
+require "active_support/all"
+
+pp "What's your name?"
+
+their_name = gets.chomp
+
+
+puts "Hello, " + their_name + "!"
